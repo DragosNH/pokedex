8927a16bac8d6c59a6011e0a0f63c84230177729
Dragoş <edragos33@gmail.com>
2026-05-21 09:06:11 +0200

    small changes

---
+++ assets/js/pokedex.js
@@ -0,0 +1,9 @@
+export default pokedex =   [
+    {
+        id: "0001",
+        name: "Bulbasaur",
+        type: "Grass, Poison",
+        description: "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+        image: "../images/pokemons/0001.png"
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+import pokedex from "./pokedex";
+
+let img = document.createElement('img');
+
+document.window.appendChild(img);
