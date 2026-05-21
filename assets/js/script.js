d0a36044466ae4dc207bd5b844b90a53face5a8f
Dragoş <edragos33@gmail.com>
2026-05-21 09:21:30 +0200

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,37 @@
-import pokedex from "./pokedex";
+pokedex =   [
+    {
+        id: "0001",
+        name: "Bulbasaur",
+        type: "Grass, Poison",
+        description: "For some time after its birth, it uses the nutrients that are packed into the seed on its back in order to grow.",
+        image: "../images/pokemons/0001.png"
+    },
+    {
+        id: "0002",
+        name: "Ivysaur",
+        type: "Grass, Poison",
+        description: "The more sunlight Ivysaur bathes in, the more strength wells up within it, allowing the bud on its back to grow larger.",
+        image: "../images/pokemons/0002.png"
+    },
+    {
+        id: "0003",
+        name: "Venusaur",
+        type: "Grass, Poison",
+        description: "While it basks in the sun, it can convert the light into energy. As a result, it is more powerful in the summertime.",
+        image: "../images/pokemons/0003.png"
+    },
+    {
+        id: "0004",
+        name: "Charmander",
+        type: "Fire",
+        description: "The flame on its tail shows the strength of its life-force. If Charmander is weak, the flame also burns weakly.",
+        image: "../images/pokemons/0004.png"
+    }
+]
 
-let img = document.createElement('img');
-img.alt = "Bulbasaur"
-img = pokedex.values()
+const main = document.querySelector(".main");
+const divContainer = document.createElement("div");
+console.log(main);
 
-document.body.appendChild(img);
+
+main.appendChild(divContainer);
