4090da3960d00b22eec05f23d352f7ff76cc63aa
Dragoş <edragos33@gmail.com>
2026-05-24 10:40:54 +0200

    added image inside dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,32 @@ pokedex =   [
         name: "Bulbasaur",
         type: "Grass, Poison",
         description: "For some time after its birth, it uses the nutrients that are packed into the seed on its back in order to grow.",
-        image: "../images/pokemons/0001.png"
+        image: "assets/images/pokemons/0001.png"
     },
     {
         id: 2,
         name: "Ivysaur",
         type: "Grass, Poison",
         description: "The more sunlight Ivysaur bathes in, the more strength wells up within it, allowing the bud on its back to grow larger.",
-        image: "../images/pokemons/0002.png"
+        image: "assets/images/pokemons/0002.png"
     },
     {
         id: 3,
         name: "Venusaur",
         type: "Grass, Poison",
         description: "While it basks in the sun, it can convert the light into energy. As a result, it is more powerful in the summertime.",
-        image: "../images/pokemons/0003.png"
+        image: "assets/images/pokemons/0003.png"
     },
     {
         id: 4,
         name: "Charmander",
         type: "Fire",
         description: "The flame on its tail shows the strength of its life-force. If Charmander is weak, the flame also burns weakly.",
-        image: "../images/pokemons/0004.png"
+        image: "assets/images/pokemons/0004.png"
     }
 ]
 
-
+// ------ Selectors ------
 const pokeForm = document.querySelector(".pokeForm")
 const search = document.querySelector(".search-bar");
 
@@ -37,8 +37,13 @@ const main = document.querySelector(".main");
 const divContainer = document.createElement("div");
 divContainer.classList = "container";
 
+// ------ Created items ------
 let title = document.createElement("h2");
+let pokeType = document.createElement("p");
+pokeType.classList = "type";
 
+let pokeImg = document.createElement("img");
+pokeImg.classList.add("pokeImg");
 
 
 pokeForm.addEventListener("submit", (e) => {
@@ -53,8 +58,15 @@ pokeForm.addEventListener("submit", (e) => {
     }
 
     title.innerText = pokemon.name;
+    pokeImg.src = pokemon.image;
+    pokeType.innerText = pokemon.type;
+
+    search.value = "";
 });
 
 
 divContainer.appendChild(title);
+divContainer.appendChild(pokeImg);
+divContainer.appendChild(pokeType);
+
 main.appendChild(divContainer);
