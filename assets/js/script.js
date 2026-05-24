297ef975f687d0f6b78f6e86b3a1103102440b56
Dragoş <edragos33@gmail.com>
2026-05-24 09:44:16 +0200

    getting started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 pokedex =   [
     {
-        id: "0001",
+        id: 1,
         name: "Bulbasaur",
         type: "Grass, Poison",
         description: "For some time after its birth, it uses the nutrients that are packed into the seed on its back in order to grow.",
         image: "../images/pokemons/0001.png"
     },
     {
-        id: "0002",
+        id: 2,
         name: "Ivysaur",
         type: "Grass, Poison",
         description: "The more sunlight Ivysaur bathes in, the more strength wells up within it, allowing the bud on its back to grow larger.",
         image: "../images/pokemons/0002.png"
     },
     {
-        id: "0003",
+        id: 3,
         name: "Venusaur",
         type: "Grass, Poison",
         description: "While it basks in the sun, it can convert the light into energy. As a result, it is more powerful in the summertime.",
         image: "../images/pokemons/0003.png"
     },
     {
-        id: "0004",
+        id: 4,
         name: "Charmander",
         type: "Fire",
         description: "The flame on its tail shows the strength of its life-force. If Charmander is weak, the flame also burns weakly.",
@@ -31,7 +31,23 @@ pokedex =   [
 
 const main = document.querySelector(".main");
 const divContainer = document.createElement("div");
-console.log(main);
+divContainer.classList = "container";
+
+let title = document.createElement("h2");
+
+/*
+const matchingUsers = users.filter(obj => obj.id === 2);
+
+// Access the first match's property
+if (matchingUsers.length > 0) {
+  const name = matchingUsers[0].name;
+  console.log(name); // Output: "Bob"
+}
+*/
+
+title.innerText = pokedex.find(pokemon => pokemon.id === 4).name;
 
 
+
+divContainer.appendChild(title);
 main.appendChild(divContainer);
